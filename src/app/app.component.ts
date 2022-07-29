b83d5134c0abe40f39c622e53c2f3bd746bba1ec
cosalamone <cosalamone@gmail.com>
2022-07-29 17:01:40 -0300

    Creando componente product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  products: Product[] = [
+    {
+      id: 1,
+      name: "product 1",
+      image: "./assets/images/product1.png",
+      price: 123
+
+    },
+    {
+      id: 2,
+      name: "product 2",
+      image: "./assets/images/product1.png",
+      price: 123
+
+    },
+    {
+      id: 3,
+      name: "product 3",
+      image: "./assets/images/product1.png",
+      price: 123
+
+    }
+  ];
 
   onLoaded(img: string) {
     console.log("log padre", img)
